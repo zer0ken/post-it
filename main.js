6afba64b0f9c269710312e5c0f2ebd1c2c22d3be
zer0ken <a37592777@gmail.com>
2021-08-17 19:53:29 +0900

    add new note on click

---
+++ main.js
@@ -0,0 +1,27 @@
+let $newNote = null
+
+$(window).ready(function () {
+    const $whiteboard = $('.whiteboard')
+    let $hold = null
+    let $newNote = $('<div class="note new hidden">hi</div>')
+    let offset = $whiteboard.offset()
+
+    console.log(offset)
+
+    $whiteboard.click(function () {
+        console.log('clicked')
+        $whiteboard.append($newNote)
+        if ($hold) {
+            $hold.removeClass('new')
+        }
+        $newNote.removeClass('hidden')
+        $hold = $newNote
+        $newNote = $(`<div class="note new hidden">hi</div>`)
+    })
+
+    $(document).mousemove(function (e) {
+        if ($hold) {
+            $hold.css({left: e.pageX - offset.left, top: e.pageY - offset.top})
+        }
+    })
+})
