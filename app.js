c20e253ed30d58a2cc30045711d12340447461fb
zer0ken <a37592777@gmail.com>
2021-08-29 23:35:49 +0900

    wow

--- app.js
+++ app.js
@@ -1,30 +1,106 @@
-class App{
+import { Point } from './point.js'
+import { Note } from './note.js'
+
+class App {
     constructor() {
         this.canvas = document.createElement('canvas')
-        this.ctx = this.canvas.getContext('2d')
+
         document.body.appendChild(this.canvas)
+        this.ctx = this.canvas.getContext('2d')
 
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1
+
+        this.mousePos = new Point()
+        this.selected = null
+        this.notes = [new Note(), new Note(), new Note()]
+
 
         window.addEventListener('resize', this.resize.bind(this), false)
         this.resize()
 
         window.requestAnimationFrame(this.animate.bind(this))
+
+        document.addEventListener('pointerdown', this.onDown.bind(this), false)
+        document.addEventListener('pointermove', this.onMove.bind(this), false)
+        document.addEventListener('pointerup', this.onUp.bind(this), false)
     }
 
     resize() {
         this.stageWidth = document.body.clientWidth
         this.stageHeight = document.body.clientHeight
-        
+
         this.canvas.width = this.stageWidth * this.pixelRatio
         this.canvas.height = this.stageHeight * this.pixelRatio
         this.ctx.scale(this.pixelRatio, this.pixelRatio)
+
+        this.ctx.shadowOffsetX = 0
+        this.ctx.shadowOffsetY = 3
+        this.ctx.shadowBlur = 6
+        this.ctx.shadowColor = 'rgba(0, 0, 0, .1)'
+        this.ctx.lineWidth = 2
+
+        for (const note of this.notes) {
+            note.resize(this.stageWidth, this.stageHeight)
+        }
     }
 
     animate() {
         window.requestAnimationFrame(this.animate.bind(this))
-        
+
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+
+        for (const note of this.notes) {
+            note.animate(this.ctx)
+        }
+
+        if (this.selected) {
+            this.ctx.fillStyle = '#ff4338'
+            this.ctx.strokeStyle = '#ff4338'
+            
+            this.ctx.beginPath()
+            this.ctx.arc(this.selected.centerPos.x, this.selected.centerPos.y, 8, 0, Math.PI * 2)
+            this.ctx.fill()
+
+            this.ctx.beginPath()
+            this.ctx.arc(this.mousePos.x, this.mousePos.y, 8, 0, Math.PI * 2)
+            this.ctx.fill()
+
+            this.ctx.beginPath()
+            this.ctx.moveTo(this.selected.centerPos.x, this.selected.centerPos.y)
+            this.ctx.lineTo(this.mousePos.x, this.mousePos.y)
+            this.ctx.stroke()
+        }
+    }
+
+    onDown(e) {
+        this.mousePos.x = e.clientX
+        this.mousePos.y = e.clientY
+        
+        for (let i = this.notes.length - 1; i >= 0; i--) {
+            const selected = this.notes[i].down(this.mousePos)
+            if (selected) {
+                this.selected = selected
+                this.notes.push(this.notes.splice(i, 1)[0])
+                break
+            }
+        }
+    }
+
+    onMove(e) {
+        this.mousePos.x = e.clientX
+        this.mousePos.y = e.clientY
+
+        for (const note of this.notes) {
+            note.move(this.mousePos)
+        }
+    }
+
+    onUp(e) {
+        this.selected = null
+
+        for (const note of this.notes) {
+            note.up()
+        }
     }
 }
 
---
+++ note.js
@@ -0,0 +1,90 @@
+import { Point } from "./point.js";
+
+const FOLLLOW_SPEED = 0.08
+const ROTATE_SPEED = 0.12
+const SPEED_REDUCE = 0.8
+const MAX_ANGLE = 30
+const FPS = 1000 / 60
+const WIDTH = 160
+const HEIGHT = 160
+
+export class Note {
+    constructor() {
+        this.pos = new Point()
+        this.target = new Point()
+        this.prevPos = new Point()
+        this.downPos = new Point()
+        this.speedPos = new Point()
+        this.startPos = new Point()
+        this.mousePos = new Point()
+        this.centerPos = new Point()
+        this.origin = new Point()
+        this.rotation = 0
+        this.sideValue = 0
+        this.isDown = false
+    }
+
+    resize(stageWidth, stageHeight) {
+        this.pos.x = Math.random() * (stageWidth - WIDTH)
+        this.pos.y = Math.random() * (stageHeight - HEIGHT)
+        this.target = this.pos.clone()
+        this.prevPos = this.pos.clone()
+    }
+
+    animate(ctx) {
+        const move = this.target.clone().sub(this.pos).reduce(FOLLLOW_SPEED)
+        this.pos.add(move)
+
+        this.centerPos = this.pos.clone().add(this.mousePos)
+
+        this.swing(ctx)
+        this.prevPos = this.pos.clone()
+    }
+
+    swing(ctx) {
+        const dx = this.pos.x - this.prevPos.x
+        const speedX = Math.abs(dx) / FPS
+        const speed = Math.min(Math.max(speedX, 0), 1)
+
+        let rotation = (MAX_ANGLE / 1) * speed
+        rotation = rotation * (dx > 0 ? 1 : -1) - this.sideValue
+        this.rotation += (rotation - this.rotation) * ROTATE_SPEED
+
+        const tempPos = this.pos.clone().add(this.origin)
+
+        ctx.save()
+        ctx.translate(tempPos.x, tempPos.y)
+        ctx.rotate(this.rotation * Math.PI / 180)
+        ctx.beginPath()
+        ctx.fillStyle = '#f4e55a'
+        ctx.fillRect(-this.origin.x, -this.origin.y, WIDTH, HEIGHT)
+        ctx.restore()
+    }
+
+    down(point) {
+        if (point.collide(this.pos, WIDTH, HEIGHT)) {
+            this.isDown = true
+            this.startPos = this.pos.clone()
+            this.downPos = point.clone()
+            this.mousePos = point.clone().sub(this.pos)
+
+            const xRatio = this.mousePos.x / WIDTH
+            this.origin.x = WIDTH * xRatio
+            this.origin.y = HEIGHT * this.mousePos.y / HEIGHT
+            this.sideValue = xRatio - .5
+            return this
+        } else {
+            return null
+        }
+    }
+
+    move(point) {
+        if (this.isDown) {
+            this.target = this.startPos.clone().add(point).sub(this.downPos)
+        }
+    }
+
+    up() {
+        this.isDown = false
+    }
+}
